ef6655d93dcab950fecbff3d3fdb999bef7a7166
Memurray <39600016+Memurray@users.noreply.github.com>
2019-03-20 14:25:06 -0400

    Add UI options
    Toggle vertical interpolation
    Change rendered text

--- src/ImageFrame.java
+++ src/ImageFrame.java
@@ -19,10 +19,12 @@ public class ImageFrame extends JFrame {
     private Point dragStart;
     private JLabel toleranceLabel;
     private JSlider toleranceSlider;
+    JCheckBoxMenuItem vInterpolateToggle;
+    JCheckBoxMenuItem textToggle;
 
     // Constructor for the frame
     public ImageFrame () {
-        super();				// call JFrame constructor
+        super("Text Replace");				// call JFrame constructor
         this.buildMenus();		// helper method to build menus
         this.buildComponents();		// helper method to set up components
         this.buildDisplay();		// Lay out the components on the display
@@ -30,7 +32,6 @@ public class ImageFrame extends JFrame {
     }
 
     private void buildMenus () {
-
         final JFileChooser fc = new JFileChooser(".");
         JMenuBar bar = new JMenuBar();
         this.setJMenuBar (bar);
@@ -38,7 +39,42 @@ public class ImageFrame extends JFrame {
         JMenuItem fileopen = new JMenuItem ("Open");
         JMenuItem saveFile = new JMenuItem ("Save to File");
         JMenuItem fileexit = new JMenuItem ("Exit");
-
+        JMenu optionsMenu = new JMenu ("Options");
+        vInterpolateToggle = new JCheckBoxMenuItem("Vertical Interpolation");
+        textToggle = new JCheckBoxMenuItem("Show Text");
+        JMenuItem setText = new JMenuItem ("Set Text");        
+        
+        setText.addActionListener(
+                new ActionListener () {
+                    public void actionPerformed (ActionEvent e) {
+                    	TextSetWindow tWindow = new TextSetWindow(view);
+                        tWindow.pack();
+                        tWindow.setVisible(true);    
+                        if(!textToggle.isSelected()) {
+                    		textToggle.setSelected(true);
+                    		view.toggleText(); 
+                    	} 
+                    }
+                }
+          );        
+        
+        vInterpolateToggle.addActionListener(
+                new ActionListener () {
+                    public void actionPerformed (ActionEvent e) {
+                    	view.toggleVint();                    	
+                    }
+                }
+          );
+        
+        textToggle.addActionListener(
+                new ActionListener () {
+                    public void actionPerformed (ActionEvent e) {
+                    	view.toggleText();  
+                    	view.repaint();
+                    }
+                }
+          );
+        
         fileopen.addActionListener(
                 new ActionListener () {
                     public void actionPerformed (ActionEvent e) {
@@ -49,7 +85,6 @@ public class ImageFrame extends JFrame {
                             try {
                                 image = ImageIO.read(file);
                             } catch (IOException e1){};
-
                             view.setImage(image);
                             toleranceLabel.setText("  " + 55 + "% Boundary Tolerance");
                             toleranceSlider.setValue(55);
@@ -84,7 +119,11 @@ public class ImageFrame extends JFrame {
         fileMenu.add(fileopen);
         fileMenu.add(saveFile);
         fileMenu.add(fileexit);
+        optionsMenu.add(vInterpolateToggle);        
+        optionsMenu.add(textToggle);        
+        optionsMenu.add(setText);
         bar.add(fileMenu);
+        bar.add(optionsMenu);
     }
 
     private void buildComponents() {
@@ -98,6 +137,14 @@ public class ImageFrame extends JFrame {
         ResetButton.addActionListener(
                 new ActionListener () {
                     public void actionPerformed (ActionEvent e) {  
+                    	if(textToggle.isSelected()) {
+                    		textToggle.setSelected(false);
+                    		view.toggleText(); 
+                    	}                    	
+                    	if(vInterpolateToggle.isSelected()) {
+                    		vInterpolateToggle.setSelected(false);
+                    		view.toggleVint();   
+                    	}
                         toleranceSlider.setValue(55);
                         view.resetImage();
                         infoLabel.setText("Original Image");
@@ -124,8 +171,7 @@ public class ImageFrame extends JFrame {
                         infoLabel.setText("Edges Detected");
                     }
                 }
-        );
-        
+        );       
         
         toleranceSlider = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 55 );
         toleranceSlider.setMajorTickSpacing(25);
@@ -143,10 +189,7 @@ public class ImageFrame extends JFrame {
 
     // This helper method adds all components to the content pane of the
     // JFrame object.  Specific layout of components is controlled here
-
     private void buildDisplay () {
-
-        // Build first JPanel
         JPanel controlPanel = new JPanel();
         GridLayout grid = new GridLayout (2, 3);
         controlPanel.setLayout(grid);
@@ -158,7 +201,6 @@ public class ImageFrame extends JFrame {
         controlPanel.add(toleranceSlider);
         infoLabel.setHorizontalAlignment(JLabel.CENTER);
         controlPanel.add(infoLabel);
-
         Container c = this.getContentPane();
         c.add(view, BorderLayout.EAST);
         c.add(controlPanel, BorderLayout.SOUTH);
@@ -172,11 +214,9 @@ public class ImageFrame extends JFrame {
                 if(isLDragging) {
 	            	isLDragging = false;  
 	                view.endSelection();
-                }
-                
+                }                
             } 
-            public void mousePressed(MouseEvent e){
-                	
+            public void mousePressed(MouseEvent e){                	
                 	dragStart = e.getPoint();               
             }
             public void mouseClicked(MouseEvent e){}
@@ -188,11 +228,9 @@ public class ImageFrame extends JFrame {
             }
             public void mouseMoved(MouseEvent e) {}
         });
-    }
- 
+    } 
 
     public BufferedImage readImage (String file) {
-
         Image image = Toolkit.getDefaultToolkit().getImage(file);
         MediaTracker tracker = new MediaTracker (new Component () {});
         tracker.addImage(image, 0);
@@ -211,7 +249,6 @@ public class ImageFrame extends JFrame {
     }
 
     public static void main(String[] argv) {
-
         JFrame frame = new ImageFrame();
         frame.pack();
         frame.setVisible(true);
