e930ce18ba78f4c67ede70ebdec0166592f27176
Memurray <39600016+Memurray@users.noreply.github.com>
2019-04-14 00:01:08 -0400

    Add Single Letter Render Method
    Can now easily build a buffered image of a single letter
    Good for testing font comparisons

--- src/BufferedImageSP.java
+++ src/BufferedImageSP.java
@@ -1,4 +1,7 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
@@ -65,6 +68,36 @@ public class BufferedImageSP extends BufferedImage{
         cop.filter(bim, newbim);
         bim = newbim;
 	}
+	
+	public void renderLetter(String c, String font, int pad) {
+		Graphics2D big = bim.createGraphics();		
+		big.setColor(Color.WHITE); 
+    	int fontSize = (int) ((height-pad*2)*1.4)+1;
+        big.setFont(new Font(font, Font.BOLD, fontSize));
+        big.drawString(c,-1,height-pad);
+	}
+	
+	
+	public int[] buildFillArray() {
+		int [] rgbim1 = new int [width*height];
+		int [] fillArray = new int [width*height];
+    	bim.getRGB (0, 0, width, height, rgbim1, 0, width);  
+    	for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++){ 
+            	int loc = retPixel(col,row);
+            	pixel p1 = new pixel(rgbim1 [loc]);
+            	if (p1.isWhite())
+            		fillArray[loc] = 1;
+            	else
+            		fillArray[loc] = -1;
+            }
+        }
+    	return fillArray;
+	}
+	
+	 private int retPixel(int col, int row) {  //common operation for getting 1-d array location from 2-d input criteria, using object width
+	    	return row*width + col;
+	    }	
 	
 	public BufferedImage getBim() {
 		return bim;
